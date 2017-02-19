369b349217e866a1f8c9fd0ff3181b7bbc6d7a26
Instrye <instrye@gmail.com>
2017-02-19 17:08:01 +0800

    Update config.inc.php

--- config.inc.php
+++ config.inc.php
@@ -1,6 +1,6 @@
 <?php
 $config['tieba'] = '百度'; //贴吧名,不带“吧”
-$config['bduss'] = '' //bduss 贴吧Cookie;
+$config['bduss'] = ''; //bduss 贴吧Cookie
 $config['ID'] = ''; //百度ID 用于辨识,无实际作用
 $config['redis_ip'] = '127.0.0.1'; //redis IP
 $config['redis_port'] = '6379'; //redis 端口
