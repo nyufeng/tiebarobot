d68db6bd441a5fff4d9f7f7f69263417e509f599
Instrye <instrye@gmail.com>
2017-02-21 09:25:15 +0800

    Update cache.php

--- include/cache.php
+++ include/cache.php
@@ -7,12 +7,37 @@ class cache{
 		$this->redis->connect($ip,$port);
 	}
 
-	public function is_process_first($id){
-		return $this->redis->sIsMember('tieba_process_first',$id);
+	public function is_process($id,$last_post){
+		if($this->redis->sIsMember('tieba_process',$id)){
+			return true;
+		}
+		$info = $this->redis->get('tieba_info:' . $id);
+		if($info && $info == $last_post){
+			return true;
+		}
+		return false;
 	}
 
-	public function add_process_first($id){
-		$this->redis->sAdd('tieba_precoss_first',$id);
+	public function is_process_first($id){
+		return $this->redis->exists('tieba_precoss_first:'.$id);
+	}
+
+	public function is_process_post($id,$post_id){
+		return $this->redis->sIsMember('tieba_precoss_first:'.$id,$post_id);
+	}
+
+	public function add_process_first($id,$post_id){
+		$this->redis->sAdd('tieba_precoss_first:'.$id,$post_id);
+	}
+
+	public function set_tieba_info($id,$uname){
+		$this->redis->set('tieba_info:'.$id,$uname);
+	}
+
+	public function add_process($id){
+		$this->redis->sAdd('tieba_process',$id);
+		$this->redis->delete('tieba_precoss_first:'.$id);
+		$this->redis->delete('tieba_info:'.$id);
 	}
 }
 //file-end
